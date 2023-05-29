e0349f4b811e77f9cb83bf6fcacd7db100614b53
422UR4H <tiagosindra@id.uff.br>
2023-05-29 15:49:19 -0300

    data-test bonus added

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
     return (
         <BrowserRouter>
             <NavContainer>
-                <Button />
+                <Button data-test="go-home-header-btn" />
                 CINEFLEX
             </NavContainer>
 
